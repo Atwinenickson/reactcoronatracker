ff72b693252a96657e1e11c6bcf6d4d5f27339b8
Atwinenickson <atwiinenicxon@gmail.com>
2020-05-05 23:24:59 +0300

    Added the line js chart

--- src/api/index.js
+++ src/api/index.js
@@ -4,22 +4,26 @@ const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
-      const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
-    const modifiedData = {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+    return {
       confirmed,
       recovered,
       deaths,
       lastUpdate,
     };
-    return modifiedData;
   } catch (error) {}
 };
 
 export const fetchDailyData = async () => {
-    try {
-        const { data } = await axios.get(`${url}/daily`)
-        console.log(data)
-    } catch (error) {
-        
-    }
-}
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date:dailyData.reportDate
+    }))
+    return modifiedData
+  } catch (error) {}
+};
